6ef57c30964109ed3533069909004a806244602d
AnhQuang198 <quanglee155198@gmail.com>
2024-01-10 00:14:16 +0700

    init kafka docker compose

--- order-service/order-domain/order-application-service/src/test/java/com/example/order/service/domain/OrderApplicationServiceTest.java
+++ order-service/order-domain/order-application-service/src/test/java/com/example/order/service/domain/OrderApplicationServiceTest.java
@@ -1,2 +1,21 @@
-package com.example.order.service.domain;public class OrderApplicationServiceTest {
-}
+//package com.example.order.service.domain;
+//
+//import com.example.order.service.domain.mapper.OrderDataMapper;
+//import com.example.order.service.domain.ports.input.service.OrderApplicationService;
+//import com.example.order.service.domain.ports.output.repository.OrderRepository;
+//import org.junit.jupiter.api.TestInstance;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+//@SpringBootTest(classes = OrderTestConfiguration.class)
+//public class OrderApplicationServiceTest {
+//    @Autowired
+//    private OrderApplicationService orderApplicationService;
+//
+//    @Autowired
+//    private OrderDataMapper orderDataMapper;
+//
+//    @Autowired
+//    private OrderRepository orderRepository;
+//}
--- order-service/order-domain/order-application-service/src/test/java/com/example/order/service/domain/OrderTestConfiguration.java
+++ order-service/order-domain/order-application-service/src/test/java/com/example/order/service/domain/OrderTestConfiguration.java
@@ -1,2 +1,45 @@
-package com.example.order.service.domain;public class OrderTestConfiguration {
-}
+//package com.example.order.service.domain;
+//
+//import com.example.order.service.domain.ports.output.message.publisher.payment.OrderCancelledPaymentRequestMessagePublisher;
+//import com.example.order.service.domain.ports.output.message.publisher.payment.OrderCreatedPaymentRequestMessagePublisher;
+//import com.example.order.service.domain.ports.output.message.publisher.restaurantapproval.OrderPaidRestaurantRequestMessagePublisher;
+//import com.example.order.service.domain.ports.output.repository.CustomerRepository;
+//import com.example.order.service.domain.ports.output.repository.RestaurantRepository;
+//import org.mockito.Mockito;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.Bean;
+//
+//@SpringBootApplication(scanBasePackages = "com.example.order.service.domain")
+//public class OrderTestConfiguration {
+//    @Bean
+//    public OrderCreatedPaymentRequestMessagePublisher orderCreatedPaymentRequestMessagePublisher() {
+//        return Mockito.mock(OrderCreatedPaymentRequestMessagePublisher.class);
+//    }
+//
+//    @Bean
+//    public OrderCancelledPaymentRequestMessagePublisher orderCancelledPaymentRequestMessagePublisher() {
+//        return Mockito.mock(OrderCancelledPaymentRequestMessagePublisher.class);
+//    }
+//
+//    @Bean
+//    public OrderPaidRestaurantRequestMessagePublisher orderPaidRestaurantRequestMessagePublisher() {
+//        return Mockito.mock(OrderPaidRestaurantRequestMessagePublisher.class);
+//    }
+//
+//    @Bean
+//    public CustomerRepository customerRepository() {
+//        return Mockito.mock(CustomerRepository.class);
+//    }
+//
+//    @Bean
+//    public RestaurantRepository restaurantRepository() {
+//        return Mockito.mock(RestaurantRepository.class);
+//    }
+//
+//    @Bean
+//    public OrderDomainService orderDomainService() {
+//        return new OrderDomainServiceImpl();
+//    }
+//
+//
+//}
